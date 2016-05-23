2b1e6e105c23651b1e987e701a4b902bcda91ba8
Patrick Snape <patricksnape@gmail.com>
2016-05-23 12:24:22 +0100

    HOTFIX: Fix linking of libvl on OSX

--- conda/build.sh
+++ conda/build.sh
@@ -32,6 +32,7 @@ cp vl/*.h $PREFIX/include/vl/
 
 # For some reason the instal_name_tool fails, so I do it manually here
 if [ "$(uname -s)" == "Darwin" ]; then
+  install_name_tool -id @rpath/libvl.dylib $PREFIX/lib/libvl.dylib
   install_name_tool -change @loader_path/libvl.dylib @rpath/../lib/libvl.dylib $PREFIX/bin/sift
   install_name_tool -change @loader_path/libvl.dylib @rpath/../lib/libvl.dylib $PREFIX/bin/mser
   install_name_tool -change @loader_path/libvl.dylib @rpath/../lib/libvl.dylib $PREFIX/bin/aib
